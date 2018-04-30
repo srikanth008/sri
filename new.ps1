bd9f9897736e8d6da8ed8041ed15952277fadb22
srikanth008 <38348592+srikanth008@users.noreply.github.com>
2018-04-30 13:42:30 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -17,7 +17,7 @@ foreach($File in $JsonFiles)
 {
 $Collectionfilepath = "$SourceFilePath\$File"
 $Environmentfilepath = "$SourceFilePath\$EnvironmentFile"
-cd newman run --disable -unicode $Collectionfilepath  -e $Environmentfilepath
+> newman run --disable -unicode $Collectionfilepath  -e $Environmentfilepath
 
 }
 
