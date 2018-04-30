f46e310a758141863599769f3f488b16e4efe58f
srikanth008 <38348592+srikanth008@users.noreply.github.com>
2018-04-30 12:38:53 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -3,8 +3,8 @@
 echo "You are in the power shell script now..."
 
 $SourceFilePath =$env:WORKSPACE
-$FilenamePostfix = ""D:\Collections\Colorado_PAYM_Inlife_SIT19.postman_collection.json""
-$EnvironmentFile =""D:\Collections\MyEE.postman_environment.json""
+$FilenamePostfix = ""D:\Collections\done_normal.postman_collection.json""
+$EnvironmentFile =""D:\Collections\done_env.json""
 
 cd C:\node_modules\newman\bin
 
