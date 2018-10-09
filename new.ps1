be592199ea4768c456d6ee50df3cc0e65a573c8d
srikanth008 <38348592+srikanth008@users.noreply.github.com>
2018-10-09 16:05:18 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -1,13 +1,11 @@
-\D:\backup\Collections 
+D:\backup\Collections 
 echo "You are in the power shell script now..."
 
 $SourceFilePath =$env:WORKSPACE
-$FilenamePostfix = "\*.postman_collection.json"
+$FilenamePostfix = "*.postman_collection.json"
 $EnvironmentFile ="done_env.json"
 
 $JsonFiles =Get-ChildItem -Path $SourcefilePath -name -Filter $FilenamePostfix | Sort-Object -Property CreationTime -Descending
-
-
 
 
 
