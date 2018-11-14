7a7639ed16a5bfda75a07d6f2ff8179faadb2b9b
srikanth008 <38348592+srikanth008@users.noreply.github.com>
2018-11-14 12:02:45 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -1,14 +1,16 @@
- echo "You are in the power shell script now..." 
-$SourceFilePath =$env:WORKSPACE 
+ 
+ echo "You are in the power shell script now..."
+ $SourceFilePath =$env:WORKSPACE
  $FilenamePostfix = "*.postman_collection.json"
  $EnvironmentFile ="done_env.json"
  $JsonFiles =Get-ChildItem -Path $SourcefilePath -name -Filter $FilenamePostfix | Sort-Object -Property CreationTime -Descending
  foreach($File in $JsonFiles)
  {
- $Collectionfilepath = "$SourceFilePath\$FilenamePostfix"
+ $Collectionfilepath = "$SourceFilePath\$File"
+
 $Environmentfilepath = "$SourceFilePath\$EnvironmentFile" 
 cd C:\npm\node_modules\newman\bin 
 node newman run $Collectionfilepath  -e $Environmentfilepath -r json 
 node newman run $Collectionfilepath  -e $Environmentfilepath -r html 
 } 
- exit $LASTEXITCODE
+exit $LASTEXITCODE
